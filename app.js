0829b08f7fd564cdded61303e700fbfddbdfeb8b
Dicky59 <pekkaarvolaine@gmail.com>
2021-02-23 13:58:02 +0200

    11.11 Health check

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+app.get('/version', (_req, res) => {
+  res.send('4') // change this string to ensure a new version deployed
+})
